590249325e2159de5ca6fb59ccce589925538ff8
jos <josbouten@gmail.com>
2015-08-05 14:58:31 +0200

    vars and labels

--- eer.py
+++ eer.py
@@ -22,16 +22,17 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 '''
-
+import warnings
+#warnings.filterwarnings("ignore", category=DeprecationWarning)
 import matplotlib.pyplot as plt
 from event import Event
 from probability import Probability
 
 class Eer(Probability):
-    def __init__(self, data, config, debug=True):
-        self.data = data
-        self.config = config
-        self.debug = debug
+    def __init__(self, thisData, thisConfig, thisDebug=True):
+        self.data = thisData
+        self.config = thisConfig
+        self.debug = thisDebug
         self.plotType = 'eer_plot'
         Probability.__init__(self, self.data, self.config, self.debug)
 
@@ -43,9 +44,15 @@ class Eer(Probability):
         axes = self.fig.add_subplot(111)
         eerData = self.computeProbabilities(self.eerFunc)
         for (metaValue, PD, PP, X) in eerData:
-            eer, score = self.computeEer(PD, PP, X)
-            pFr, = axes.plot(X, PP, 's-', label="P(pros), %s Eer: %0.2f%s at %0.2f" % (metaValue, eer * 100, '%', score))
-            pFa, = axes.plot(X, PD, 'o-', label="P(def), %s" % metaValue)
+            try:
+                eer, score = self.computeEer(PD, PP, X)
+            except Exception:
+                print "Eer: problem computing EER for %s" % metaValue
+                pFr, = axes.plot(X, PP, 's-', label="P(pros), %s Eer: undefined" % (metaValue))
+                pFa, = axes.plot(X, PD, 'o-', label="P(def), %s" % metaValue)
+            else:
+                pFr, = axes.plot(X, PP, 's-', label="P(pros), %s Eer: %0.2f%s at %0.2f" % (metaValue, eer * 100, '%', score))
+                pFa, = axes.plot(X, PD, 'o-', label="P(def), %s" % metaValue)
         plt.legend()
         axes.set_title("P(defense) and P(prosecution) for '%s'" % self.data.getTitle())
         plt.xlabel('Threshold (raw score)')
